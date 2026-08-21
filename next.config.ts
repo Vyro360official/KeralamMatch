23e0b725d6c716e2090bc1c6cea2f1059d9abd95
Vyro360 <vyro360official@gmail.com>
2026-08-21 14:39:57 +0200

    fix: configure Content Security Policy to allow Google reCAPTCHA and Firebase Auth

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://checkout.razorpay.com https://www.clarity.ms;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  font-src 'self' https://fonts.gstatic.com;
-  img-src 'self' blob: data: https://res.cloudinary.com https://www.google-analytics.com https://lh3.googleusercontent.com;
-  connect-src 'self' https://www.google-analytics.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://checkout.razorpay.com;
-  frame-src https://checkout.razorpay.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://checkout.razorpay.com https://www.clarity.ms https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com/;
+  font-src 'self' https://fonts.gstatic.com https://www.gstatic.com/;
+  img-src 'self' blob: data: https://res.cloudinary.com https://www.google-analytics.com https://lh3.googleusercontent.com https://www.gstatic.com/ https://www.google.com/;
+  connect-src 'self' https://www.google-analytics.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebaseinstallations.googleapis.com https://checkout.razorpay.com https://www.google.com/recaptcha/;
+  frame-src https://checkout.razorpay.com https://www.google.com/recaptcha/ https://recaptcha.google.com/;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
