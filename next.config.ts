039381b7fa1edb76b34b86a18ef61d8479891b48
Vyro360 <vyro360official@gmail.com>
2026-08-21 14:58:08 +0200

    fix: add apis.google.com and vercel domains to CSP

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://checkout.razorpay.com https://www.clarity.ms https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com/;
-  font-src 'self' https://fonts.gstatic.com https://www.gstatic.com/;
-  img-src 'self' blob: data: https://res.cloudinary.com https://www.google-analytics.com https://lh3.googleusercontent.com https://www.gstatic.com/ https://www.google.com/;
-  connect-src 'self' https://www.google-analytics.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebaseinstallations.googleapis.com https://checkout.razorpay.com https://www.google.com/recaptcha/;
-  frame-src https://checkout.razorpay.com https://www.google.com/recaptcha/ https://recaptcha.google.com/;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://checkout.razorpay.com https://www.clarity.ms https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://apis.google.com/ https://vercel.live https://*.vercel.live;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com/ https://vercel.live https://*.vercel.live;
+  font-src 'self' https://fonts.gstatic.com https://www.gstatic.com/ https://vercel.live https://*.vercel.live;
+  img-src 'self' blob: data: https://res.cloudinary.com https://www.google-analytics.com https://lh3.googleusercontent.com https://www.gstatic.com/ https://www.google.com/ https://vercel.live https://*.vercel.live;
+  connect-src 'self' https://www.google-analytics.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebaseinstallations.googleapis.com https://checkout.razorpay.com https://www.google.com/recaptcha/ https://apis.google.com/ https://vercel.live https://*.vercel.live;
+  frame-src https://checkout.razorpay.com https://www.google.com/recaptcha/ https://recaptcha.google.com/ https://vercel.live https://*.vercel.live;
+  manifest-src 'self' https://vercel.com https://*.vercel.com;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
